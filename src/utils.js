51a9b8c4f0ca98a2697d402e277500f6ba2973bb
Iskander Broere <iskanderbroere@gmail.com>
2018-03-07 01:41:38 +0100

    rm log

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,6 @@ function getUserId (ctx) {
   const Authorization = ctx.request.get('Authorization')
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '')
-    console.log('token', token)
     if (token.length > 7) {
       const { userId } = jwt.verify(token, process.env.APP_SECRET)
       return userId
